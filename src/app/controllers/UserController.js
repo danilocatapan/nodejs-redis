e3fa1c5a895ad0340e422bff64f4d5549c822ef6
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-07-06 22:02:33 -0300

    feat: send email

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,4 +1,5 @@
 import * as faker from 'faker';
+import Mail from '../lib/Mail';
 
 export default {
   async store(req, res) {
@@ -9,6 +10,13 @@ export default {
       email,
       password: faker.internet.password(15, false)
     };
+
+    await Mail.sendMail({
+      from: 'Red-Potion <contact@red-potion.com>',
+      to: `${name} <${email}>`,
+      subject: 'User register',
+      html: `Hello ${name}, welcome to Red-Potion.`
+    })
     
     return res.json(user);
   }
